7482df43b78567bbdb97ef1a429a15b4fb08f46a
Trent B <trent.bullard@gmail.com>
2016-03-23 12:49:14 -0500

    begin implementation of bootstrap admin template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require sb-admin-2.js
+//= require bootstrap.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
