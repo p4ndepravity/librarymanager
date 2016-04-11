fe6b45f0cbfaf444c6861673736ec53b96f91d2a
Trent B <trent.bullard@gmail.com>
2016-04-11 13:29:27 -0500

    bump; fix seeds for dev/test again

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,29 +41,6 @@ Book.create([
 {title: 'Down and Out in Paris and London' ,author_last_name: 'Orwell' ,author_first_name: 'George' ,rating: '7'  ,isbn_10: '015626224X', isbn_13: '9780156262248', genre_id: genres.third.id   ,publish_date: '1972-03-01 00:00:00', is_fiction: '1'},
 {title: 'Animal Farm'                      ,author_last_name: 'Orwell' ,author_first_name: 'George' ,rating: '7'  ,isbn_10: '0451524667', isbn_13: '9780451524669', genre_id: genres.fourth.id  ,publish_date: '1986-07-01 00:00:00', is_fiction: '1'},
 {title: '1984'                             ,author_last_name: 'Orwell' ,author_first_name: 'George' ,rating: '8'  ,isbn_10: '0451524934', isbn_13: '9780451524935', genre_id: genres.fourth.id  ,publish_date: '1961-01-01 00:00:00', is_fiction: '1'}])
-(1..100).each do |i|
-  title_array = [Faker::Commerce.product_name,Faker::Company.name,Faker::Company.bs,Faker::Commerce.department,Faker::Company.catch_phrase]
-  Book.create(title: title_array[rand(0..4)].titlecase, 
-              author_last_name: Faker::Name.last_name, 
-              author_first_name: Faker::Name.first_name, 
-              rating: rand(1..10), 
-              isbn_10: Faker::Number.number(10), 
-              isbn_13: Faker::Number.number(13), 
-              genre_id: rand(1..7),
-              publish_date: Faker::Time.between(100.years.ago, Time.now, :all),
-              is_fiction: rand(0..1))
-end
-
-Patron.create last_name: 'Cena', first_name: 'John', street_address: Faker::Address.street_address, city: 'Austin', state: 'TX', zip: Faker::Address.postcode, phone_number: Faker::PhoneNumber.cell_phone
-(1..100).each do |i|
-  Patron.create(last_name: Faker::Name.last_name, 
-                first_name: Faker::Name.first_name, 
-                street_address: Faker::Address.street_address,
-                city: Faker::Address.city, 
-                state: Faker::Address.state_abbr, 
-                zip: Faker::Address.postcode, 
-                phone_number: Faker::PhoneNumber.cell_phone)
-end
 
 TransactionType.create([
 {name: 'check out'},
@@ -72,14 +49,51 @@ TransactionType.create([
 {name: 'return'   },
 {name: 'XBLANKX'  }])
 
-Book.all.each do |book|
-  patron = Patron.find(rand(1..Patron.count))
-  book.transactions.create patron_id: patron.id, transaction_type_id: 1, created_at: Faker::Time.between(3.weeks.ago, 3.hours.ago, :all)
-end
+if Rails.env.development? or Rails.env.test? then
+  (1..100).each do |i|
+    title_array = [Faker::Commerce.product_name,Faker::Company.name,Faker::Company.bs,Faker::Commerce.department,Faker::Company.catch_phrase]
+    Book.create(title: title_array[rand(0..4)].titlecase, 
+                author_last_name: Faker::Name.last_name, 
+                author_first_name: Faker::Name.first_name, 
+                rating: rand(1..10), 
+                isbn_10: Faker::Number.number(10), 
+                isbn_13: Faker::Number.number(13), 
+                genre_id: rand(1..7),
+                publish_date: Faker::Time.between(100.years.ago, Time.now, :all),
+                is_fiction: rand(0..1))
+  end
 
-(1..(Book.count/2)).each do |i|
-  t = Transaction.find((i*2)-1)
-  book = t.book
-  patron = t.patron
-  Transaction.create patron_id: patron.id, book_id: book.id, transaction_type_id: 2, created_at: Faker::Time.between(t.created_at, Time.now, :all)
+  Patron.create last_name: 'Cena', first_name: 'John', street_address: Faker::Address.street_address, city: 'Austin', state: 'TX', zip: Faker::Address.postcode, phone_number: Faker::PhoneNumber.cell_phone
+  (1..100).each do |i|
+    Patron.create(last_name: Faker::Name.last_name, 
+                  first_name: Faker::Name.first_name, 
+                  street_address: Faker::Address.street_address,
+                  city: Faker::Address.city, 
+                  state: Faker::Address.state_abbr, 
+                  zip: Faker::Address.postcode, 
+                  phone_number: Faker::PhoneNumber.cell_phone)
+  end
+
+  Book.all.each do |book|
+    patron = Patron.find(rand(1..Patron.count))
+    book.transactions.create patron_id: patron.id, transaction_type_id: 1, created_at: Faker::Time.between(3.weeks.ago, 3.hours.ago, :all)
+  end
+
+  (1..(Book.count/2)).each do |i|
+    t = Transaction.find((i*2)-1)
+    book = t.book
+    patron = t.patron
+    Transaction.create patron_id: patron.id, book_id: book.id, transaction_type_id: 2, created_at: Faker::Time.between(t.created_at, Time.now, :all)
+  end
+else
+  Patron.create([
+  {last_name: 'Rapp', first_name: 'Mitchell', street_address: '3433 Bayview Dr', city: 'Chesapeake', state: 'MD', zip: '20732', phone_number: '999-999-9999'}])
+
+  patron = Patron.first
+
+  patron.transactions.create book_id: 23, transaction_type_id: 1, created_at: (Time.now - 16.days)
+  patron.transactions.create book_id: 1, transaction_type_id: 1, created_at: (Time.now - 12.days)
+  patron.transactions.create book_id: 5, transaction_type_id: 1, created_at: (Time.now - 1.days)
+  patron.transactions.create book_id: 3, transaction_type_id: 1, created_at: (Time.now - 1.month)
+  patron.transactions.create book_id: 3, transaction_type_id: 2, created_at: (Time.now - 2.weeks)
 end
